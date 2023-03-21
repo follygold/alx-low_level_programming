d7b804e9cf991a30c10e0a44816672db6fbd0ce1
Follygold <afolakebaiyewu@yahoo.com>
2023-03-21 16:34:50 -0700

    char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always returns 0
- *
+ * main - debugging example
+ * Return: 0
  */
 int main(void)
 {
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char *_putchar = "_putchar";
+
+	while (*_putchar)
+	{
+		_putchar(*_putchar);
+		_putchar++;
+	}
 	_putchar('\n');
 	return (0);
 }
