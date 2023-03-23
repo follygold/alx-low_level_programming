1f572c83058acae04c3aee122ed174abe1617196
Follygold <afolakebaiyewu@yahoo.com>
2023-03-23 09:52:28 -0700

    print num

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,16 +5,19 @@
  *
  * @n: number.
  */
-void print_number(int n)
-{
-        if (n < 0)
-        {
-                _putchar('-');
-                n = -n;
-        }
-        if (n / 10 != 0)
-        {
-                print_number(n / 10);
-        }
-        _putchar(n % 10 + '0');
+void print_number(int n) {
+    int divisor = 1;
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
+    while (n / divisor >= 10) {
+        divisor *= 10;
+    }
+    while (divisor > 0) {
+        int digit = n / divisor;
+        _putchar('0' + digit);
+        n -= digit * divisor;
+        divisor /= 10;
+    }
 }
