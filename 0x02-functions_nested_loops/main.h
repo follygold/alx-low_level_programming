f84a861d69a93a593469bee3e2383def508e3e2a
Follygold <afolakebaiyewu@yahoo.com>
2023-03-22 18:20:53 -0700

    main xs tbl

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,10 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void times_table(void);
+/**
+ * add - adds stuff up
+ *
+ * @int: integers to add
+ * Return: returns a sum
+ */
