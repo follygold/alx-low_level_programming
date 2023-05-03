44512be69225e86453ddee3d3b73d637a8ea14af
follygold <afolakebaiyewu@yahoo.com>
2023-05-03 01:41:14 -0500

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,46 +1,49 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
- * at a given position
- * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
- *
- * Return: pointer to the new node, or NULL
- */
+* insert_nodeint_at_index - inserts a new node at a given position
+* @head: double pointer to the head of the listint_t linked list
+* @idx: the index of the list where the new node should be added
+* @n: element/property n of the node to be added
+* Return: address of the new element (SUCCESS), or
+* NULL if it failed (FAILURE), or
+* NULL if is not possible to add the new node at index @idx
+*/
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *new;
-	listint_t *temp = *head;
-
-	new = malloc(sizeof(listint_t));
-	if (!new || !head)
-		return (NULL);
-
-	new->n = n;
-	new->next = NULL;
+	unsigned int i = 0;
+	listint_t *current_node = *head, *new_node_ptr;
 
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		new_node_ptr = malloc(sizeof(listint_t));
+
+		if (!new_node_ptr)
+			return (NULL);
+		new_node_ptr->n = n;
+		new_node_ptr->next = *head;
+		*head = new_node_ptr;
+		return (new_node_ptr);
 	}
 
-	for (i = 0; temp && i < idx; i++)
+	while (current_node && ((i + 1) != idx))
 	{
-		if (i == idx - 1)
-		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
-		}
-		else
-			temp = temp->next;
+		current_node = current_node->next;
+		i++;
 	}
 
+	if ((i + 1) == idx)
+	{
+		new_node_ptr = malloc(sizeof(listint_t));
+
+		if (!new_node_ptr)
+			return (NULL);
+		new_node_ptr->n = n;
+		new_node_ptr->next = current_node->next;
+		current_node->next = new_node_ptr;
+		return (new_node_ptr);
+	}
 	return (NULL);
 }
-
